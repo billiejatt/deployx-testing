6eedadc2f40159a6b7322d04fcfb32b404bab690
Billie Jatt <153736256+billiejatt@users.noreply.github.com>
2024-12-13 09:02:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  * Plugin Name:       SEO X - AIOS Checker
  * Plugin URI:        https://fectodigital.com
  * Description:       This plugin will link your WordPress application with all the performance of SEO Ponts provided by Fecto Digital
- * Version:           1.0.3
+ * Version:           1.0.1
  * Author:            Billie Jutt
  * Author URI:        https://www.fectodigital.com
  * Text Domain:       seox
