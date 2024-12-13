4d7c24c9e5e82f8f76e5d83caaafbba7aa605c3c
Billie Jatt <153736256+billiejatt@users.noreply.github.com>
2024-12-13 08:08:45 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * SEO X
+ *
+ * @package           SEOX
+ * @author            Billie Jutt
+ * @link              http://www.fectodigital.com/
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SEO X - AIOS Checker
+ * Plugin URI:        https://fectodigital.com
+ * Description:       This plugin will link your WordPress application with all the performance of SEO Ponts provided by Fecto Digital
+ * Version:           1.0.0
+ * Author:            Billie Jutt
+ * Author URI:        https://www.fectodigital.com
+ * Text Domain:       seox
+ *
+ */
