bcd9c60794100686987853c2639c4327c09e5a89
Billie Jatt <153736256+billiejatt@users.noreply.github.com>
2024-12-15 01:45:55 +0500

    Update index.php
    Signed-off-by: Billie Jatt <153736256+billiejatt@users.noreply.github.com>

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * @link              http://www.fectodigital.com//
  *
  * @wordpress-plugin
- * Plugin Name:       SEO X - AIOS Checker
+ * Plugin Name:       SEO X - AIOS Checker 1
  * Plugin URI:        https://fectodigital.com
  * Description:       This plugin will link your WordPress application with all the performance of SEO Ponts provided by Fecto Digital
  * Version:           1.0.1
